f3d22ae510ea66d80dfc032d82f1a366134d48b2
Fisher <truefisherking@gmail.com>
2024-07-09 18:43:59 -0400

    Added other TFT States and switch statement

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let locations = [
     "Fairfield, CT",
     "Hamden, CT",
     "Milford, CT",
-    "Orange, CT"
+    "Orange, CT",
 ];
 let salesPerson = "Fisher";
 
@@ -20,20 +20,20 @@ let salesNames = [];
 let qrImages = [];
 let controlText = locations[0];
 let stateLocation = controlText.substring(controlText.indexOf(",") + 2, controlText.length);
-let imgControlText = controlText.replace(", ","-").toLowerCase();
-let selectLocation = createElementPlus("select",controller,null,{id:"location",name:"location"},null);
+let imgControlText = controlText.replace(", ", "-").toLowerCase();
+let selectLocation = createElementPlus("select", controller, null, { id: "location", name: "location" }, null);
 
-theTitle.innerText = `TFT Card - ${controlText.replace(",","")} - ${salesPerson}`;
+theTitle.innerText = `TFT Card - ${controlText.replace(",", "")} - ${salesPerson}`;
 getStateName();
 
 for (let i = 0; i < locations.length; i++) {
     const location = locations[i];
-    createElementPlus("option",selectLocation,null,{value: `${location}`},location);
+    createElementPlus("option", selectLocation, null, { value: `${location}` }, location);
 }
 
-let printCards = createElementPlus("button",controller,"print-card",null,"Print Card");
-printCards.addEventListener("click",function(){
-    print()
+let printCards = createElementPlus("button", controller, "print-card", null, "Print Card");
+printCards.addEventListener("click", function () {
+    print();
 });
 salesInput.addEventListener('click', () => {
     salesInput.select();
@@ -42,84 +42,101 @@ salesInput.addEventListener("keyup", updateSalesName);
 salesInput.addEventListener("change", updateSalesName);
 selectLocation.addEventListener("change", updateLocation);
 
-for (let i = 0; i < 12; i++) {  
-    let card = createElementPlus("div",cards,"card",null,null);
-    let images = createElementPlus("div",card,"images",null,null);
+for (let i = 0; i < 12; i++) {
+    let card = createElementPlus("div", cards, "card", null, null);
+    let images = createElementPlus("div", card, "images", null, null);
     // Create tft-logo
-    createElementPlus("img",images,null,{src:`images/Town Fair Logo_Red Back.png`,alt:`tft-logo`},null);
+    createElementPlus("img", images, null, { src: `images/Town Fair Logo_Red Back.png`, alt: `tft-logo` }, null);
     // Create QR Code Logo
-    let qrImage = createElementPlus("img",images,null,{src:`images/qr-codes/${imgControlText}.png`,alt:`QR Code`},null);
+    let qrImage = createElementPlus("img", images, null, { src: `images/qr-codes/${imgControlText}.png`, alt: `QR Code` }, null);
     qrImages.push(qrImage);
-    let textDiv = createElementPlus("div",card,"text",null,null);
-    let top = createElementPlus("div",textDiv,"top",null,null);
+    let textDiv = createElementPlus("div", card, "text", null, null);
+    let top = createElementPlus("div", textDiv, "top", null, null);
     // Name of sales person
-    let mid = createElementPlus("div",textDiv,"mid",null,salesPerson);
+    let mid = createElementPlus("div", textDiv, "mid", null, salesPerson);
     salesNames.push(mid);
     // Create top paragraph
-    createElementPlus("p",top,null,null,"Guaranteed Lowest Price");
+    createElementPlus("p", top, null, null, "Guaranteed Lowest Price");
     // Create next two paragraphs
-    let tftParagraph = createElementPlus("p",top,null,null,`Town Fair Tire Centers <br>of ${stateName} LLC <br>${controlText}`);
+    let tftParagraph = createElementPlus("p", top, null, null, `Town Fair Tire Centers <br>of ${stateName} LLC <br>${controlText}`);
     tftParagraphs.push(tftParagraph);
-    let stars = createElementPlus("div",top,"stars",null,null);
+    let stars = createElementPlus("div", top, "stars", null, null);
     for (let i = 0; i < 5; i++) {
         // create star image
-        createElementPlus("img",stars,null,{src:"images/star.png",alt:"star"},null);
+        createElementPlus("img", stars, null, { src: "images/star.png", alt: "star" }, null);
         // createElementPlus("div",stars,"star",null,null);
     };
-    let bottom = createElementPlus("div",textDiv,"bottom",null,null);
+    let bottom = createElementPlus("div", textDiv, "bottom", null, null);
     // Create Salesman line
-    createElementPlus("p",bottom,null,null,"Your Salesman");
-    let footer = createElementPlus("footer",card,null,null,null);
+    createElementPlus("p", bottom, null, null, "Your Salesman");
+    let footer = createElementPlus("footer", card, null, null, null);
     // Create footer paragraph
-    createElementPlus("p",footer,null,null,"Satisfied with your service? Let us know on");
+    createElementPlus("p", footer, null, null, "Satisfied with your service? Let us know on");
     // Create Google image in bottom paragraph
-    createElementPlus("img",footer,null,{src:"images/google_logo-667x400.png",alt:"Google Logo"},null);
+    createElementPlus("img", footer, null, { src: "images/google_logo-667x400.png", alt: "Google Logo" }, null);
 }
 
-function createElementPlus(elementType,appendToElement,theClass,theAttributes,theText) {
+function createElementPlus(elementType, appendToElement, theClass, theAttributes, theText) {
     let createdElement = document.createElement(elementType);
-    appendToElement.appendChild(createdElement);  
+    appendToElement.appendChild(createdElement);
     if (theClass) {
         createdElement.classList.add(theClass);
     }
     for (const key in theAttributes) {
         if (Object.hasOwnProperty.call(theAttributes, key)) {
             const attName = theAttributes[key];
-            createdElement.setAttribute(key,attName);
+            createdElement.setAttribute(key, attName);
         }
     }
     createdElement.innerHTML = theText;
     return createdElement;
 };
-function updateSalesName(){
+function updateSalesName() {
     salesPerson = salesInput.value;
-    salesNames.forEach(function(sn){
+    salesNames.forEach(function (sn) {
         sn.innerText = salesPerson;
     });
-    theTitle.innerText = `TFT Card - ${controlText.replace(",","")} - ${salesPerson}`;
+    theTitle.innerText = `TFT Card - ${controlText.replace(",", "")} - ${salesPerson}`;
 };
-function updateLocation(){
+function updateLocation() {
     controlText = locations[selectLocation.options.selectedIndex];
-    stateLocation = controlText.substring(controlText.indexOf(",") + 2,controlText.length );
+    stateLocation = controlText.substring(controlText.indexOf(",") + 2, controlText.length);
     getStateName();
-    tftParagraphs.forEach(function(tftP){
+    tftParagraphs.forEach(function (tftP) {
         tftP.innerHTML = `Town Fair Tire Centers <br>of ${stateName} LLC <br>${controlText}`;
     });
-    imgControlText = controlText.replace(", ","-").toLowerCase();
-    qrImages.forEach(function(qrImg){
-        qrImg.setAttribute("src",`images/qr-codes/${imgControlText}.png`);
+    imgControlText = controlText.replace(", ", "-").toLowerCase();
+    qrImages.forEach(function (qrImg) {
+        qrImg.setAttribute("src", ``);
     });
-    
-    theTitle.innerText = `TFT Card - ${controlText.replace(",","")} - ${salesPerson}`;
+
+    theTitle.innerText = `TFT Card - ${controlText.replace(",", "")} - ${salesPerson}`;
 };
-function getStateName(){
-    if (stateLocation == "CT") {
-        stateName = "Connecticut";
-    } else if (stateLocation == "NH"){
-        stateName = "New Hampshire";
-    } else if (stateLocation == "MA"){
-        stateName = "Massachusetts";
-    } else if (stateLocation == "NY"){
-        stateName = "New York";
+function getStateName() {
+    switch (stateLocation) {
+        case "CT":
+            stateName = "Connecticut";
+            break;
+        case "NH":
+            stateName = "New Hampshire";
+            break;
+        case "NY":
+            stateName = "New York";
+            break;
+        case "ME":
+            stateName = "Maine";
+            break;
+        case "MA":
+            stateName = "Massachusetts";
+            break;
+        case "RI":
+            stateName = "Rhode Island";
+            break;
+        case "VT":
+            stateName = "Vermont";
+            break;
+
+        default:
+            break;
     }
 };
